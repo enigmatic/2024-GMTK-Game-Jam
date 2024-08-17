350b7b9eab4ef233f268a1b00f1cd9b53b900cc8
Marshall Couch <marshall.couch@gmail.com>
2024-08-17 13:05:02 -0400

    some camera stuff

---
+++ scripts/Camera.gd
@@ -0,0 +1,24 @@
+extends Camera2D
+
+var is_dragging = false
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _input(event):
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_MIDDLE:
+		if event.is_released():
+			is_dragging = false
+		if event.is_pressed():
+			is_dragging = true
+	if event is InputEventMouseMotion and is_dragging:
+		position -=event.relative
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_WHEEL_UP:
+		zoom *= 1.1
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
+		zoom /= 1.1
