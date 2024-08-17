b1ef4239042ecf62f4234d90f054f2fcdd9f629b
Matt Couch <mcouch@gmail.com>
2024-08-17 13:44:09 -0500

    Tree drinks water!

--- scripts/underground/RootSection.gd
+++ scripts/underground/RootSection.gd
@@ -6,17 +6,20 @@ class_name RootSection
 @export var growthRate: float = 100;
 @export var width = 10;
 @export var widthGrowth = 2;
+@export var dropSpeed:float = 10;
 
 
 @onready var line = $Line2D
 @onready var collisionShape = $Area2D/CollisionShape2D
 @onready var collisionArea = $Area2D;
 @onready var consumeTimer = $ConsumeTimer;
+@onready var main_drop = $WaterDrop
+@onready var drops = $Drops
 
 var parent: RootSection = null;
 var touching: UndergroundCollidable = null;
 var segment = SegmentShape2D.new();
-var _done = false;
+var _doneGrowing = false;
 
 func _ready():
 	if parent:
@@ -32,33 +35,20 @@ func _ready():
 	if parent:
 		parent.added_child();
 
-func _physics_process(delta):
-	if (!_done):
+func _process(delta):
+	if (!_doneGrowing):
 		var start_point = line.get_point_position(1);
 		var point = start_point.move_toward(target, growthRate * delta);
 
-		# Don't need to look for intersections if we don't let the user try and make them
-		#
-		#var space_state = self.get_parent().get_world_2d().direct_space_state
-		#
-		## use global coordinates, not local to node		
-		#var query = PhysicsRayQueryParameters2D.create(line.global_position + start_point, line.global_position + point, 4294967295, [get_collision_RID()] );
-		#query.collide_with_areas = true
-		#query.hit_from_inside = true
-		#var result = space_state.intersect_ray(query)
-		#
-		#if result:
-			#target = point;
-			
 		if (is_equal_approx(point.x, target.x) && is_equal_approx(point.y, target.y)):
-			_done = true;
+			_doneGrowing = true;
 			point = target;
 			
 		line.set_point_position(1, point);
 		segment.b = point;
 		
 		#make room for more growth
-		if _done:
+		if _doneGrowing:
 			var room_to_grow = source.move_toward(target, 1);
 			segment.a = room_to_grow;
 			
@@ -67,17 +57,28 @@ func _physics_process(delta):
 			
 			if touching: 
 				consumeTimer.start();
+	for drop in drops.get_children():
+		var source_global = to_global(source);
+		drop.position = drop.position.move_toward(source_global, dropSpeed * delta);
+		if (is_equal_approx(drop.position.x, source_global.x) && is_equal_approx(drop.position.y, source_global.y)):
+			#TODO: move into parent
+			if parent:
+				parent.start_drop();
+			else:
+				pass # TODO: tree get's water?
+			drop.queue_free();
+			
 			
 
 func nearest_point_to(x:Vector2):
 	var p2 = target;
-	if (!_done):
+	if (!_doneGrowing):
 		p2  = line.get_point_position(1);
 	
 	return Geometry2D.get_closest_point_to_segment(x, source, p2);
 	
 func get_end_point():
-	if (_done and !touching):
+	if (_doneGrowing and !touching):
 		return line.get_point_position(1);
 	else:
 		return line.get_point_position(0);
@@ -97,5 +98,12 @@ func _on_consume_timer_timeout():
 		if touching.type() == 'water':
 			if (touching.consume(1, to_global(target))):
 				consumeTimer.start();
+				start_drop();
 			else:
 				touching = null;
+
+func start_drop():
+	var newDrop:Sprite2D = main_drop.duplicate();
+	newDrop.position = to_global(target);
+	newDrop.visible = true;
+	drops.add_child(newDrop, true);
