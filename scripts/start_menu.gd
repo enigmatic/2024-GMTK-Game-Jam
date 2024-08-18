7347fe1d44fb88c009c38a5e2fbecb3cad0ed60c
Marshall Couch <marshall.couch@gmail.com>
2024-08-18 14:38:52 -0400

    sound settings now save between plays

--- scripts/start_menu.gd
+++ scripts/start_menu.gd
@@ -2,9 +2,11 @@ extends Panel
 
 
 signal reset
+var settings:Settings
+@onready var music_slider: Slider = $VBoxContainer/MusicSlider
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	load_settings()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -24,6 +26,8 @@ func _input(event):
 
 func _on_music_slider_value_changed(value):
 	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Soundtrack"),linear_to_db(value/100.0))
+	settings.music_level = value
+	ResourceSaver.save(settings,"user://settings.tres")
 
 
 func _on_play_button_button_up():
@@ -32,3 +36,14 @@ func _on_play_button_button_up():
 
 func _on_reset_button_button_up():
 	reset.emit()
+
+func load_settings():
+	if ResourceLoader.exists("user://settings.tres"):
+		settings = ResourceLoader.load("user://settings.tres")
+		if settings.music_level>-1:
+			music_slider.value = settings.music_level
+			AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Soundtrack"),linear_to_db(settings.music_level/100.0))
+	else:
+		settings = Settings.new()
+			
+
