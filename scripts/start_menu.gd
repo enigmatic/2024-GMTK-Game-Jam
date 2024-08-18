1c553e69963b068e2a23f28f213f408287eda2ee
Marshall Couch <marshall.couch@gmail.com>
2024-08-18 14:14:43 -0400

    added some stubs for the menu

--- scripts/start_menu.gd
+++ scripts/start_menu.gd
@@ -1,6 +1,7 @@
 extends Panel
 
 
+signal reset
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -18,3 +19,16 @@ func _input(event):
 		else:
 			show()
 			get_tree().paused = true
+
+
+
+func _on_music_slider_value_changed(value):
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Soundtrack"),linear_to_db(value/100.0))
+
+
+func _on_play_button_button_up():
+	hide()
+	get_tree().paused = false
+
+func _on_reset_button_button_up():
+	reset.emit()
